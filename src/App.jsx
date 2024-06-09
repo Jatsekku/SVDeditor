382db62006352353543e85e1adec9e2315e16736
Jakub <ghosthidiro@gmail.com>
2024-06-09 15:07:11 +0200

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,18 @@ import RegisterEditor from './components/RegisterEditor/RegisterEditor'
 import TreeView from './components/TreeView/TreeView'
 import styles from './App.module.css'
 
-function App() {
-	return <h1>Hello world!</h1>
+const App = () => {
+	return (
+		<div className={styles.appContainer}>
+			<div className={styles.sidebar}>
+				<button className={styles.sidebarButton}>Open File</button>
+				<div className={styles.treeView}>{/* Tree View Content */}</div>
+			</div>
+			<div className={styles.content}>
+				<RegisterEditor />
+			</div>
+		</div>
+	)
 }
 
 export default App
