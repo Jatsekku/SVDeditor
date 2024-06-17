9f2e8cf379866938987fe1a59ce38ee31305fb4f
Jatsekku <jatsekku2@gmail.com>
2024-06-18 00:32:22 +0200

    feat: Add opening file functionality (dialog-open)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { open } from "@tauri-apps/api/dialog"
 import RegisterEditor from './components/RegisterEditor/RegisterEditor'
 import TreeView from './components/TreeView/TreeView'
 import styles from './App.module.css'
+
+async function pickFile() {
+	const selectedPath = await open({
+		multiple: false,
+		title: "Select SVD file",
+	})
+	console.log(selectedPath)
+}
 
 const App = () => {
 	return (
 		<div className={styles.appContainer}>
 			<div className={styles.sidebar}>
-				<button className={styles.sidebarButton}>Open File</button>
+				<button onClick={pickFile} className={styles.sidebarButton}>Open File</button>
 				<div className={styles.treeView}>{/* Tree View Content */}</div>
 			</div>
 			<div className={styles.content}>
